298caa352dadf8e87f89864a5214a489cbf7b907
honda.tatsuya <honda.tatsuya@donuts.ne.jp>
2018-10-23 10:48:38 +0900

    refactor: ノート作成を共通化

--- assets/ts/objects/Note.ts
+++ assets/ts/objects/Note.ts
@@ -2,6 +2,7 @@ import { Fraction, IFraction } from "../math";
 import GraphicObject from "./GraphicObject";
 import { GUID } from "../util";
 import { observable } from "mobx";
+import Chart from "../stores/Chart";
 
 interface INoteEditorProps {
   time: number;
@@ -41,8 +42,31 @@ export default class Note extends GraphicObject {
   @observable
   data: INoteData;
 
-  constructor(data: INoteData) {
+  constructor(data: INoteData, chart: Chart) {
     super();
     this.data = data;
+    const noteType = chart.musicGameSystem!.noteTypeMap.get(data.type)!;
+
+    // 不要カスタムプロパティの削除と新規カスタムプロパティの追加
+    const newProps: any = {};
+    for (const prop of noteType.customProps) {
+      if (prop.key in data.customProps) {
+        newProps[prop.key] = data.customProps[prop.key];
+      } else {
+        newProps[prop.key] = prop.defaultValue;
+      }
+    }
+    this.data.customProps = newProps;
+
+    // editorProps.color
+    if (noteType.editorProps.color === "$laneColor") {
+      this.data.editorProps.color = Number(
+        chart.musicGameSystem!.laneTemplateMap.get(
+          chart.timeline.laneMap.get(data.lane)!.templateName
+        )!.color
+      );
+    } else {
+      this.data.editorProps.color = Number(noteType.editorProps.color);
+    }
   }
 }
