479da396cb17be07ee3bfa60cff3c44a540ef373
Ishizaki Taishi <ishizaki.taishi@donuts.ne.jp>
2018-10-29 20:31:17 +0900

    perf: ノートのレンダラーをキャッシュする

--- src/objects/NoteRendererResolver.ts
+++ src/objects/NoteRendererResolver.ts
@@ -3,22 +3,28 @@ import NoteRenderer, { INoteRenderer } from "./NoteRenderer";
 import Pixi from "../containers/Pixi";
 
 export default class NoteRendererResolver {
+  private static renderers = new WeakMap<any, any>();
+
   static resolve(note: INote): INoteRenderer {
     const noteType = Pixi.instance!.injected.editor!.currentChart!.musicGameSystem!.noteTypeMap.get(
       note.data.type
     )!;
 
-    // console.log(noteType.rendererReference);
-
+    // デフォルトレンダラー
     if (noteType.renderer === "default") return NoteRenderer;
 
-    if (noteType.rendererReference) {
-      return {
+    // レンダラー作成済み
+    if (this.renderers.has(noteType.rendererReference)) {
+      return this.renderers.get(noteType.rendererReference);
+    } else if (noteType.rendererReference) {
+      // レンダラー作成
+      const renderer = {
         getBounds: NoteRenderer.getBounds,
         render: NoteRenderer.render,
-
-        customRender: noteType.rendererReference as any
-      } as any;
+        customRender: noteType.rendererReference
+      };
+      this.renderers.set(noteType.rendererReference, renderer);
+      return renderer;
     } else return NoteRenderer;
   }
 }
