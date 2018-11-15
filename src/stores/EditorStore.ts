fe7de712f26d8e6c0e95cba2165a181d120985ea
honda.tatsuya <honda.tatsuya@donuts.ne.jp>
2018-11-15 18:29:59 +0900

    fix: bug

--- src/stores/EditorStore.ts
+++ src/stores/EditorStore.ts
@@ -148,7 +148,7 @@ export default class Editor implements IStore {
         properties: ["openFile", "multiSelections"],
         filters: this.dialogFilters
       },
-      this.openFiles
+      (paths: any) => this.openFiles(paths)
     );
   }
 
