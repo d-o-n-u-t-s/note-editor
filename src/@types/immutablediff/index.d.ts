e78266a5ca169ca9be24f92d1cbedea217f66c42
Ishizaki Taishi <ishizaki.taishi@donuts.ne.jp>
2019-01-25 16:18:50 +0900

    clean: 不要なファイルの整理

--- src/@types/immutablediff/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "immutablediff" {
-  function immutablediff(obj: any, b: any): any;
-  namespace immutablediff {
-
-  }
-  export = immutablediff;
-}
--- src/@types/immutablepatch/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "immutablepatch" {
-  function immutablepatch(value: any, patches: any): any;
-  namespace immutablepatch {
-
-  }
-  export = immutablepatch;
-}
