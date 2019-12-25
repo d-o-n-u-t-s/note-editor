7cf6e3632f79a2eb6b02433aadd7d2b67c704e95
Ishizaki Taishi <ishizaki.taishi@donuts.ne.jp>
2019-12-25 17:47:03 +0900

    update: ExtensionUtility に fs と Editor の取得を追加した

--- src/utils/ExtensionUtility.ts
+++ src/utils/ExtensionUtility.ts
@@ -1,5 +1,7 @@
-import { inverseLerp, lerp, Vector2, Fraction } from "../math";
+import * as fs from "fs";
+import { Fraction, inverseLerp, lerp, Vector2 } from "../math";
 import { NoteRecord } from "../objects/Note";
+import Editor from "../stores/EditorStore";
 import { guid } from "../utils/guid";
 
 export class ExtensionUtility {
@@ -9,6 +11,10 @@ export class ExtensionUtility {
   NoteRecord = NoteRecord;
   guid = guid;
   Fraction = Fraction;
+  fs = fs;
+  getEditor() {
+    return Editor.instance!;
+  }
 }
 
 export default new ExtensionUtility();
