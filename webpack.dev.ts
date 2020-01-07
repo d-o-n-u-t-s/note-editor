c6fdead3560bdedee7bb290dc78a8062057d6714
Ishizaki Taishi <ishizaki.taishi@donuts.ne.jp>
2020-01-07 18:22:34 +0900

    update: react と tsconfig を更新

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import * as merge from "webpack-merge";
+import merge from "webpack-merge";
 import common from "./webpack.common";
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
