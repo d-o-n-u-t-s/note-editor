e1f8c0cdc1fbb827b46b87d318a15361177850bb
MineshimaMitsumasa <mineshima.mitsumasa@donuts.ne.jp>
2020-04-27 14:15:10 +0900

    update: スナップを小節の分割数似合わせる

--- src/containers/VisTimeline.tsx
+++ src/containers/VisTimeline.tsx
@@ -143,17 +143,18 @@ class VisTimeline extends InjectedComponent {
   ) {
     const chart = this.injected.editor.currentChart!;
     const measureSize = Math.abs(chart.timeline.measures[0].beginTime - chart.timeline.measures[1].beginTime) * 1000;
+    const measureDivSize = measureSize / this.injected.editor.setting.measureDivision;
 
     // 0以下にしない
     item.start = Math.max(item.start.valueOf() as number, 0);
 
     // 近いところにスナップ
-    item.start = Math.floor(item.start.valueOf() as number / measureSize) * (measureSize + 1);
-    item.end =  Math.floor(item.end!.valueOf() as number / measureSize) * (measureSize + 1);
+    item.start = Math.floor(item.start.valueOf() as number / measureDivSize) * (measureDivSize + 1);
+    item.end =  Math.floor(item.end!.valueOf() as number / measureDivSize) * (measureDivSize + 1);
 
     // 最小サイズ以下にしない
-    if((item.end!.valueOf() as number - item.start.valueOf() as number) < measureSize) {
-        item.end = item.start + measureSize;
+    if((item.end!.valueOf() as number - item.start.valueOf() as number) < measureDivSize) {
+        item.end = item.start + measureDivSize;
     }
 
     const n = this.itemMap.get(item.id as number)!;
